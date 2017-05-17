ab8159bf85cb58c3eb09d3923ff076d642301fc8
Cyberia 2045 <sagansociety86@gmail.com>
2017-05-17 17:09:24 -0400

    added minor styling and formatting.  Created a blog form

--- app.rb
+++ app.rb
@@ -1,11 +1,12 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'sqlite3'
 
-set :database, 'sqlite3:app.sqlite3'
+set :database, 'sqlite3:application.sqlite3'
 set :sessions, true
 
 require './models'
 
 get '/' do
-	
+	erb :index
 end
